eaa057e2d7102aeb557adbfc8925d9f7fae21dec
AlwaysFocus <eliwhalen3@gmail.com>
2021-02-07 18:33:16 -0700

    FocusStore Backend
    Complete implementation of the backend of Focus Store. Uses Strapi, Magic, and Stripe

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8db09fe16540bf7d9662073b5af66f85'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9c318ec-f3a5-40b4-b43e-2f6381fe0794'
+};
